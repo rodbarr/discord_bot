c04f0fdd972314dcde2c30ff530693530ff46ef1
Rodrigo <rodrigo.barrientos2310@gmail.com>
2020-10-05 22:16:20 -0500

    set up the data

--- codewarsapi.py
+++ codewarsapi.py
@@ -1,18 +1,11 @@
 import json
 import requests
-
-API_TOKEN = "?access_key=V93QtNVwDczdWg24u29"
-URL = "https://www.codewars.com/api/v1/users/"
-
-user = input("Enter the user you want to look for: ")
-
-a = requests.get(f"{URL}{user}{API_TOKEN}")
-data = a.json()
-
-print(data['name'], data['honor'])
+import config
 
 
-# def getUser(user):
-#     a = requests.get(f"{URL}{user}{API_TOKEN}")
-#     data = a.json()
-#     return data
+def getUser(user):
+    a = requests.get(f"{config.URL}{user}{config.API_TOKEN}")
+    data = a.json()
+    return data
+
+print(getUser("rodbarr"))
