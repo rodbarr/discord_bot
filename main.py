b0907bb67af279a75a37608fdc37bbe938cc6ab9
Rodrigo <rodrigo.barrientos2310@gmail.com>
2020-10-06 15:02:32 -0500

    Basic commands

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import discord
+from discord.ext import commands
 import config as conf
 import codewarsapi as api
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_message(m):
-    m.content = m.content.lower()
-    if m.author == client.user:
-        return
-    if m.content.startswith(f"{conf.TRIGGER}help"):
-        await m.channel.send(f"{conf.TRIGGER}user <username> will get the data of the user")
-    if m.content.startswith(f"{conf.TRIGGER}user"):
-        await m.channel.send("user data")
-        #user = m.content.remove(f"{conf.TRIGGER}user")
-        #await m.channel.send(user)
-client.run(conf.DISCORD_TOKEN)
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command()
+async def test(ctx, *, arg):
+    await ctx.send(arg)
+
+@bot.command(help = "This command displays the stats of the specified user", usage = "<codewars username>")
+async def user(ctx, *, arg):
+    cw = api.getUser(arg)
+    await ctx.send(f"User: {arg}\n Honor: {cw['honor']}\n Rank: {cw['ranks']['overall']['name']}")
+    
+bot.run(conf.DISCORD_TOKEN)
