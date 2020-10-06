27c599352a4e623e4e8fd7fbd003c7dc7d644c5c
Rodrigo <rodrigo.barrientos2310@gmail.com>
2020-10-05 23:51:18 -0500

    Start of the bot connection and some commands

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import discord
-import config
+import config as conf
 import codewarsapi as api
 
-print(api.getUser("rodbarr"))
+client = discord.Client()
+
+@client.event
+async def on_message(m):
+    m.content = m.content.lower()
+    if m.author == client.user:
+        return
+    if m.content.startswith(f"{conf.TRIGGER}help"):
+        await m.channel.send(f"{conf.TRIGGER}user <username> will get the data of the user")
+    if m.content.startswith(f"{conf.TRIGGER}user"):
+        await m.channel.send("user data")
+        #user = m.content.remove(f"{conf.TRIGGER}user")
+        #await m.channel.send(user)
+client.run(conf.DISCORD_TOKEN)
